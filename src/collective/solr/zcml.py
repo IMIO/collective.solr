691b3fe5879e64f40aebc331b34058615891aa84
Chris Adam <chris.adam@imio.be>
2025-10-07 10:05:14 +0200

    Fixed unicode error for python 2

--- src/collective/solr/zcml.py
+++ src/collective/solr/zcml.py
@@ -9,20 +9,20 @@ class ISolrConnectionConfigDirective(Interface):
     """Directive which registers a Solr connection config"""
 
     host = schema.ASCIILine(
-        title="Host",
-        description="The host name of the Solr instance to be used.",
+        title=u"Host",
+        description=u"The host name of the Solr instance to be used.",
         required=True,
     )
 
     port = schema.Int(
-        title="Port",
-        description="The port of the Solr instance to be used.",
+        title=u"Port",
+        description=u"The port of the Solr instance to be used.",
         required=True,
     )
 
     base = schema.ASCIILine(
-        title="Base",
-        description="The base prefix of the Solr instance to be used.",
+        title=u"Base",
+        description=u"The base prefix of the Solr instance to be used.",
         required=True,
     )
 
